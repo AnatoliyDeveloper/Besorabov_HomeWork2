bab3fc0edcfc8ae5a955115e01afce9277e01587
AnatoliyDeveloper <sheroryaru@gmail.com>
2016-12-11 22:14:13 +0200

    First commit

---
+++ app/src/main/java/com/devandroid/menuapplication/LastActivity.java
@@ -0,0 +1,17 @@
+package com.devandroid.menuapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Anatoliy on 11.12.2016.
+ */
+
+public class LastActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.last_layout);
+    }
+}
---
+++ app/src/main/java/com/devandroid/menuapplication/MainActivity.java
@@ -0,0 +1,67 @@
+package com.devandroid.menuapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Message;
+import android.service.carrier.CarrierMessagingService;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.telecom.PhoneAccount;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void callLastActivity(View view){
+        Intent intent = new Intent(this, LastActivity.class);
+        startActivity(intent);
+    }
+
+    public void callPhoneBook(View view){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        startActivity(intent);
+    }
+
+    public void callMessageEditor(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("sms:"));
+        startActivity(intent);
+    }
+}
